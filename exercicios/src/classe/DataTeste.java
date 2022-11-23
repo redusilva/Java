3cba54244bd44c30d742b2c8c6947d8f91599649
Rodrigo Eduardo da Silva <rodrigoeduardo347@gmail.com>
2022-11-23 07:06:40 -0300

    Entendendo mais sobre Construtores

--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -3,17 +3,17 @@ package classe;
 public class DataTeste {
 
     public static void main(String[] args) {
-        Data d1 = new Data();
-        d1.dia = 20;
-        d1.mes = 11;
-        d1.ano = 2022;
+        Data d1 = new Data(20, 11, 2022);
         
         var d2 = new Data();
         d2.dia = 2;
         d2.mes = 10;
         d2.ano = 2002;
         
+        var d3 = new Data();
+        
         System.out.printf("Hoje eh %s.\n", d1.obterDataFormatada());
         System.out.printf("Nasci em %s.\n", d2.obterDataFormatada());
+        System.out.printf("Data aleatoria %s.\n", d3.obterDataFormatada());
     }
 }
--- exercicios/src/classe/ProdutoTeste.java
+++ exercicios/src/classe/ProdutoTeste.java
@@ -3,10 +3,7 @@ package classe;
 public class ProdutoTeste {
 
     public static void main(String[] args) {
-        Produto p1 = new Produto();
-        p1.nome = "Notebook";
-        p1.preco = 4525.78;
-        p1.desconto = 0.25;
+        Produto p1 = new Produto("Notebook", 4525.78, 0.25);
         
         Produto p2 = new Produto();
         p2.nome = "Caneta Azul";
